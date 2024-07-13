3838573f0b9b1b7c0707ab06c3213b5e6b3f128f
MichKili <83296432+MichKili@users.noreply.github.com>
2024-07-13 14:46:30 +0200

    Example tests for DFS partition palindromes (backtracking)

--- src/main/java/task/Solution.java
+++ src/main/java/task/Solution.java
@@ -1,6 +1,22 @@
 package task;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
 
+    public List<List<String>> partition(String s) {
+        int length = s.trim().length();
+        validateInput(s, length);
 
+        return new ArrayList<List<String>>();
+    }
+
+    private static void validateInput(String s, int length) {
+        if (s == null ) throw new IllegalStateException("Given string is out of scope");
+
+        if(length < 1 || length > 20){
+            throw new IllegalStateException("Given string is out of scope");
+        }
+    }
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,18 +1,44 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import task.Solution;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @ExtendWith(MockitoExtension.class)
 class SolutionTest {
 
+    @InjectMocks
+    private Solution solution;
 
     @Test
-    void shouldTest() {
+    void shouldMakeTwoPartitionOfPalindromes() {
         //given
+        String word = "aab";
 
         //when
+        List<List<String>> partition = solution.partition(word);
 
         //then
+        assertThat(partition).hasSize(2)
+                .contains(List.of("a", "a", "b"), List.of("aa", "b"));
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"exceedingStringLengthWord"})
+    void shouldThrowExceptionWhenStringLengthIsOutOfScope(String value) {
+        //when then
+        assertThatThrownBy(() -> solution.partition(value))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Given string is out of scope");
     }
 
 }
