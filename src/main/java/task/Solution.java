483183a54daf5e32d65f899c5cb5706d9aa4c3cc
MichKili <83296432+MichKili@users.noreply.github.com>
2024-07-22 16:19:29 +0200

    Whole task (only 1)

--- src/main/java/task/Solution.java
+++ src/main/java/task/Solution.java
@@ -1,5 +1,48 @@
 package task;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Solution {
+
+    private static final String URL_DOMAIN = "://short.com/";
+    private static final int MAX_SIZE_KEYWORD = 20;
+
+    private final Map<String, String> urlMap = new ConcurrentHashMap<>(10);
+
+
+    public String shortGivenUrl(String url, String keyword) {
+        URL shortenerUrl;
+
+        if (keyword.trim().length() == 0) {
+            throw new IllegalStateException("Keyword can not be empty string");
+        }
+
+        if (keyword.length() > MAX_SIZE_KEYWORD) {
+            throw new IllegalStateException("Keyword can not have more than 20 characters");
+        }
+
+        synchronized (urlMap) {
+            if (urlMap.containsKey(keyword)) {
+                throw new IllegalStateException("Some url already contains this keyword");
+            }
+
+            urlMap.put(keyword, url);
+        }
+
+        try {
+            shortenerUrl = new URL(url);
+        } catch (MalformedURLException e) {
+            throw new IllegalStateException("Url can not be empty");
+        }
+
+        String protocol = shortenerUrl.getProtocol();
+        StringBuilder stringBuilder = new StringBuilder(protocol);
+        stringBuilder.append(URL_DOMAIN);
+        stringBuilder.append(keyword);
+        return stringBuilder.toString();
+    }
 
 }
--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,13 +1,8 @@
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import task.Solution;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -19,12 +14,87 @@ class SolutionTest {
     private Solution solution;
 
     @Test
-    void test() {
+    void shouldReturnShortenerURLHttp() {
         //given
+        String url = "http:///loooooong.com/somepath";
+        String keyword = "MY-NEW-PATH";
 
         //when
+        String shortenerUrl = solution.shortGivenUrl(url, keyword);
 
         //then
+        assertThat(shortenerUrl).isNotEmpty();
+        assertThat(shortenerUrl).isNotEqualTo(url);
+        assertThat(shortenerUrl).isEqualTo("http://short.com/MY-NEW-PATH");
+    }
+
+    @Test
+    void shouldReturnShortenerURLHttps() {
+        //given
+        String url = "https:///loooooong.com/somepath";
+        String keyword = "MY-NEW-PATH";
+
+        //when
+        String shortenerUrl = solution.shortGivenUrl(url, keyword);
+
+        //then
+        assertThat(shortenerUrl).isNotEmpty();
+        assertThat(shortenerUrl).isNotEqualTo(url);
+        assertThat(shortenerUrl).isEqualTo("https://short.com/MY-NEW-PATH");
+    }
+
+    @Test
+    void shouldThrowExceptionWhenKeywordIsEmpty() {
+        //given
+        String url = "http:///loooooong.com/somepath";
+        String keyword = "";
+
+        //when //then
+        assertThatThrownBy(() -> solution.shortGivenUrl(url, keyword))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Keyword can not be empty string");
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUrlIsEmpty() {
+        //given
+        String url = "";
+        String keyword = "test";
+
+        //when //then
+        assertThatThrownBy(() -> solution.shortGivenUrl(url, keyword))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Url can not be empty");
+    }
+
+
+    @Test
+    void shouldThrowExceptionWhenKeywordHasMoreThan20Chars() {
+        //given
+        String url = "http:///loooooong.com/somepath";
+        String keyword = "1234567890123456789011";
+
+        //when //then
+        assertThatThrownBy(() -> solution.shortGivenUrl(url, keyword))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Keyword can not have more than 20 characters");
+
+    }
+
+    @Test
+    void shouldThrowExceptionWhenKeywordIsTheSameButUrlIsDifferent(){
+        //given
+        String url1 = "http:///loooooong.com/somepath";
+        String url2 = "http:///loooooong.com/somepath2";
+        String keyword = "keyword";
+
+        //when
+        solution.shortGivenUrl(url1, keyword);
+
+        // then
+        assertThatThrownBy(() -> solution.shortGivenUrl(url2, keyword))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Some url already contains this keyword");
     }
 
 }
